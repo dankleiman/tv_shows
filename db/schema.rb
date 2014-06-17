259336abd75a33a0a506a02f25ab4886cdc7142a
Dan Kleiman <info@dankleiman.com>
2014-06-17 11:08:21 -0400

    View character description on tv show show page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140616201811) do
+ActiveRecord::Schema.define(version: 20140617145950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140616201811) do
     t.string  "role",               null: false
     t.string  "actor"
     t.integer "television_show_id"
+    t.text    "description"
   end
 
   create_table "television_shows", force: true do |t|
---
+++ spec/features/view_character_details_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+feature 'user views character info on show page', %Q{
+  As a site visitor
+  I want to view the details for a TV show
+  So I can learn more about it
+} do
+
+
+
+# Acceptance Criteria:
+# * I can see a the show's title, network, the years it ran, and a synopsis
+# * For each character, I can see the character's name, actor's name, and the character's description
+
+  it 'adds a character' do
+    attrs = {
+        title: 'Game of Thrones',
+        network: 'HBO',
+        years: '2011-',
+        synopsis: 'Seven noble families fight for control of the mythical land of Westeros.'
+      }
+    show = TelevisionShow.create(attrs)
+
+    character = Character.create(
+      role: 'Tyrion Lannister',
+      actor: 'Peter Dinklage',
+      description: "Tyrion is the third and youngest son of Lord Tywin and Lady Joanna Lannister. He is a dwarf. Because of his unusual stature Tyrion has been a victim of mockery his entire life, with people overlooking his noble birth in order to tease and taunt him. He has also suffered physical indignities in a world not built for someone his size.",
+      television_show: show)
+
+    visit television_show_path(show.id)
+
+    expect(page).to have_content character.role
+    expect(page).to have_content character.actor
+    expect(page).to have_content character.description
+    expect(page).to have_content show.title
+
+  end
+end
+
+
